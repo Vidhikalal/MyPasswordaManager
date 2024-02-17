5d043b01f4330673632c29d563c3b18551362947
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-17 02:34:45 -0500

    Edited DatabaseAccess

--- src/main/java/ca/sheridancollege/kalalv/assignment2/databases/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/kalalv/assignment2/databases/DatabaseAccess.java
@@ -1,8 +1,8 @@
-//package ca.sheridancollege.kalalv.assignment2.databases;
-//
-//import ca.sheridancollege.kalalv.assignment2.beans.Password;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface DatabaseAccess extends CrudRepository<Password, Long> {
-//
-//}
+package ca.sheridancollege.kalalv.assignment2.databases;
+
+import ca.sheridancollege.kalalv.assignment2.beans.Password;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DatabaseAccess extends CrudRepository<Password, Long> {
+
+}
