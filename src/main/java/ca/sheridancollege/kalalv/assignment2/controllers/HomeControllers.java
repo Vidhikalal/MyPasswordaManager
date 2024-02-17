54a92bbf6edc3353a6ae37ef77db16c0294d414e
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-17 16:36:01 -0500

    Created html pages

--- src/main/java/ca/sheridancollege/kalalv/assignment2/controllers/HomeControllers.java
+++ src/main/java/ca/sheridancollege/kalalv/assignment2/controllers/HomeControllers.java
@@ -1,6 +1,6 @@
 package ca.sheridancollege.kalalv.assignment2.controllers;
 
-import ca.sheridancollege.kalalv.assignment2.databases.DatabaseAccess;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
