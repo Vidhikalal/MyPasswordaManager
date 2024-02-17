1c9d9796ef17fb4d43e6759228d27ae62668c751
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-17 17:49:54 -0500

    DatabaseAccess crud op

--- src/main/java/ca/sheridancollege/kalalv/assignment2/databases/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/kalalv/assignment2/databases/DatabaseAccess.java
@@ -4,7 +4,7 @@ import ca.sheridancollege.kalalv.assignment2.beans.Password;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface DatabaseAccess extends CrudRepository<Password, Long> {
-//
-//}
+@Repository
+public interface DatabaseAccess extends CrudRepository<Password, Long> {
+
+}
