35ddb73902f45709cceb8b9ecac90ede712ddbb5
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-17 02:45:46 -0500

    Edited Controllers

--- src/main/java/ca/sheridancollege/kalalv/assignment2/controllers/HomeControllers.java
+++ src/main/java/ca/sheridancollege/kalalv/assignment2/controllers/HomeControllers.java
@@ -1,10 +1,18 @@
 package ca.sheridancollege.kalalv.assignment2.controllers;
 
+import ca.sheridancollege.kalalv.assignment2.databases.DatabaseAccess;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class HomeControllers {
 
+public DatabaseAccess databaseAccess;
+@GetMapping("/")
+public String ReturnIndex(Model model){
+    model.addAttribute("passwordList", databaseAccess.findAll());
+    return "index";
+}
 
 }
