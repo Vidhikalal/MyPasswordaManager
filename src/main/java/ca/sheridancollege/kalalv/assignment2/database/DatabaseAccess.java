59c1c338caca5b3fe53eaaa895821a07207a5687
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-18 02:44:10 -0500

    Mapping and Services

--- src/main/java/ca/sheridancollege/kalalv/assignment2/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/kalalv/assignment2/database/DatabaseAccess.java
@@ -1,8 +1,9 @@
 package ca.sheridancollege.kalalv.assignment2.database;
-
+import ca.sheridancollege.kalalv.assignment2.controllers.HomeControllers;
 import ca.sheridancollege.kalalv.assignment2.beans.Password;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
